ee552139795b4aacd78d0ec773e2e32502c323d0
angelica winkler <caw400@nyu.edu>
2018-06-13 22:35:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,6 @@ def speak_to_grandma (phrase)
     return "HUH?! SPEAK UP, SONNY"
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  else
     return "I LOVE YOU TOO PUMPKIN!"
 end
